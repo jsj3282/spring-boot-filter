958dd4c5ecc60abe15577f92eb748568e036a22a
jsj3282 <jeongseonju15@gmail.com>
2022-01-31 23:20:49 +0900

    first commit

---
+++ src/main/java/com/example/filter/dto/User.java
@@ -0,0 +1,14 @@
+package com.example.filter.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+}
